f7cd0864f41c2c29bdd433b1c02261a3a86b4a69
YahiaElGamal <96386403+yahiaElGamal98@users.noreply.github.com>
2022-02-01 21:09:58 +0200

    adding uart to card

--- Card/MCAL/UART/UART.c
+++ Card/MCAL/UART/UART.c
@@ -26,13 +26,17 @@ void UART_init()
 	//select UBRRH and shift right the baud rate to set the remaining bits
 	UBRRH_UCSRC= (uint8_t)(BAUD_RATE>> REG_SIZE);
 	//enable transmit and receive of UART and receive complete interrupts
-	UCSRB= (HIGH<<RXEN) | (HIGH<<TXEN) | (HIGH<<RXCIE);  
+	UCSRB= (HIGH<<RXEN) | (HIGH<<TXEN);  
 	// select UCSRC register and configure 8 bits of data in a frame with 1 stop bit and user configured parity
 	UBRRH_UCSRC = (HIGH<<URSEL) | (PARITY_MODE<<UPM0) | (STOP_MODE<<USBS) | (HIGH<<UCSZ1) | (HIGH <<UCSZ0); 
 }
 void setRxCompleteCallback(void (*callback)(void))
 {
 	RxCompleteCallback=callback;
+}
+uint8_t UART_returnRxStatus(void)
+{
+	return (UCSRA&(HIGH<<RXC))>>RXC);
 }
 void UART_receiveChar(uint8_t* u8_val)
 {
